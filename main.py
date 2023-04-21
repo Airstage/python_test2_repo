ca879cb682170cf72266024966b49987d16cd1fb
Admin <airstage@mail.ru>
2023-04-21 18:27:01 +0300

    Added a new function for time managment.

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
+from datetime import datetime as dt
+
+
+def what_time_is_now():
+    print(dt.now())
+
+
 print('Привет, мир!')
 
 print('Меня зовут Кирилл.')
@@ -5,3 +12,5 @@ print('В этом проекте я тестирую возможности VCS
 print('В дальнейшем добавлю несколько функций.')
 
 print('Конец.')
+
+what_time_is_now()
